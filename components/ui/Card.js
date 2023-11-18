3725b3a2c3983a907e404e6c6a83f3818c2a58f9
uzair-399 <129755851+uzair-399@users.noreply.github.com>
2023-11-18 23:14:12 +0500

    Add files via upload

---
+++ components/ui/Card.js
@@ -0,0 +1,23 @@
+import { View, StyleSheet, Dimensions } from "react-native";
+import Colors from "../../constants/colors";
+function Card({ children, style }) {
+  return <View style={[styles.Card]}>{children}</View>;
+}
+export default Card;
+const deviceWidth = Dimensions.get("window").width;
+const styles = StyleSheet.create({
+  Card: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: deviceWidth < 380 ? 12 : 24,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
